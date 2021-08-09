1c4aaa27b0c3db548d0cd92d8fed2b7fe9212142
Nalinga <87804198+it21020094@users.noreply.github.com>
2021-08-09 16:02:50 +0000

    Done

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,20 @@
 #include <stdio.h>
 
 int main() {
+
+  int s_mark1, s_mark2, sum;
+  float avg;
+
+  printf("Input mark 1 :");
+  scanf("%d", &s_mark1);
+  printf("input mark 2 :");
+  scanf("%d", &s_mark2);
+
+  sum = s_mark1 + s_mark2;
+  printf("sum is :%d\n", sum);
+
+  avg = sum / 2;
+  printf("average is : %.2f", avg);
   
   return 0;
 }
-
